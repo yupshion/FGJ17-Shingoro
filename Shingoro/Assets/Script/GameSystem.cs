17024dc5183292aeda7aadadb6a994597b472cc6
takaratt <takahiko@numasen.ac.jp>
2017-08-19 16:09:14 +0900

    別シーンを追加（作業シーン）

---
+++ Shingoro/Assets/Script/GameSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSystem : MonoBehaviour {
+
+    float GameTime;
+    public Text GameTimeText;
+
+    // Use this for initialization
+    void Start () {
+        GameTime = 60f;
+        GameTimeText.text = "" + GameTime.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameTime -= 1f * Time.deltaTime;
+        GameTimeText.text = ((int)GameTime).ToString();
+
+    }
+}
