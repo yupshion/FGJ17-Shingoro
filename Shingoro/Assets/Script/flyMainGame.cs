1e306b217709c1ac5f26087831c555275c4c2f51
takaratt <takahiko@numasen.ac.jp>
2017-08-20 11:53:52 +0900

    no message

--- Shingoro/Assets/Script/flyMainGame.cs
+++ Shingoro/Assets/Script/flyMainGame.cs
@@ -7,11 +7,9 @@ public class flyMainGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
+
 	//	AkSoundEngine.PostEvent("BGM_title", gameObject);
 
-=======
->>>>>>> 47012f9fb4418db986f9d545084f97c4843ad487
     
 	}
 	
@@ -20,12 +18,12 @@ public class flyMainGame : MonoBehaviour {
 		
 	}
 
-<<<<<<< HEAD
+
+   // public void movescene()
+    //{
+        //	AkSoundEngine.PostEvent("next", gameObject);
+   // }
     public void movescene() {
-	//	AkSoundEngine.PostEvent("next", gameObject);
-=======
-    public void movescene() {
->>>>>>> 47012f9fb4418db986f9d545084f97c4843ad487
         SceneManager.LoadScene("GameMain");
     }
 }
